5a649195c9b3ca82e2fe24a570976aa27da8a84f
Glinkis <glinkis@gmail.com>
2021-05-21 20:56:01 +0200

    display error type as title

--- app/components/route-error.tsx
+++ app/components/route-error.tsx
@@ -10,7 +10,7 @@ export function RouteError({ error }: Props) {
   return (
     <div className="center-content">
       <section className="error">
-        <h1 className="title">App Error</h1>
+        <h1 className="title">{error.name}</h1>
         <p className="message">{error.message}</p>
       </section>
     </div>
