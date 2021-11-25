d13fef6f7efe88584e41cd3369ab53913bfdfa25
Glinkis <glinkis@gmail.com>
2021-11-25 20:01:07 +0100

    remove formatting

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,7 +1,6 @@
 import ReactDOMServer from "react-dom/server"
 import type { EntryContext } from "remix"
 import { RemixServer } from "remix"
-import prettier from "prettier"
 
 export default function handleRequest(
   request: Request,
@@ -9,19 +8,9 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
-  let markup =
-    "<!DOCTYPE html>" +
-    ReactDOMServer.renderToString(
-      <RemixServer context={remixContext} url={request.url} />
-    )
-
-  if (process.env.NODE_ENV !== "production") {
-    // Serve a pretty version of the HTML in development.
-    markup = prettier.format(markup, {
-      parser: "html",
-      htmlWhitespaceSensitivity: "ignore",
-    })
-  }
+  let markup = ReactDOMServer.renderToString(
+    <RemixServer context={remixContext} url={request.url} />
+  )
 
   responseHeaders.set("Content-Type", "text/html")
 
