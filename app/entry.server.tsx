713dc12c43ab4c238d868d70b574b6481b2ddfe5
Victor Glindås <victor.glindas@paf.com>
2021-11-26 18:59:05 +0100

    set up etag headers

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,6 +1,7 @@
 import ReactDOMServer from "react-dom/server"
-import type { EntryContext } from "remix"
+import type { EntryContext, HandleDataRequestFunction } from "remix"
 import { RemixServer } from "remix"
+import etag from "etag"
 
 export default function handleRequest(
   request: Request,
@@ -12,10 +13,38 @@ export default function handleRequest(
     <RemixServer context={remixContext} url={request.url} />
   )
 
+  responseHeaders.set("ETag", etag(markup))
   responseHeaders.set("Content-Type", "text/html")
+
+  if (request.headers.get("If-None-Match") === responseHeaders.get("ETag")) {
+    return new Response("", {
+      status: 304,
+      headers: responseHeaders,
+    })
+  }
 
   return new Response(markup, {
     status: responseStatusCode,
     headers: responseHeaders,
   })
+}
+
+export let handleDataRequest: HandleDataRequestFunction = async (
+  response,
+  { request }
+) => {
+  let body = await response.text()
+
+  if (request.method.toLowerCase() === "get") {
+    response.headers.set("etag", etag(body))
+
+    if (request.headers.get("If-None-Match") === response.headers.get("ETag")) {
+      return new Response("", {
+        status: 304,
+        headers: response.headers,
+      })
+    }
+  }
+
+  return response
 }
